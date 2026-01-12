af3fb2f721144f6c439ce1231788bdbf2ba268ba
sakupi01 <saku@email.sakupi01.com>
2026-01-12 17:11:58 +0900

    [fix] SSR settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import netlify from "@astrojs/netlify";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://frontend-conf.fukuoka.jp",
-  output: "static",
+  output: "server",
+  adapter: netlify(),
   integrations: [sitemap()],
 });
